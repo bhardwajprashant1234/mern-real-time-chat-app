80226223a67aaae15cf7cbda9a10efa62dc606f5
Prashant Bhardwaj <bhrdwjprashant@gmail.com>
2024-02-17 16:05:14 +0530

    deployment

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,6 +1,6 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
-//import { getReceiverSocketId, io } from "../socket/socket.js";
+import { getReceiverSocketId, io } from "../socket/socket.js";
 
 export const sendMessage = async (req, res) => {
 	try {
@@ -35,18 +35,18 @@ export const sendMessage = async (req, res) => {
 		await Promise.all([conversation.save(), newMessage.save()]);
 
 		// SOCKET IO FUNCTIONALITY WILL GO HERE
-	//	const receiverSocketId = getReceiverSocketId(receiverId);
-	//	if (receiverSocketId) {
-			// io.to(<socket_id>).emit() used to send events to specific client
-	//		io.to(receiverSocketId).emit("newMessage", newMessage);
-	//	}
+		const receiverSocketId = getReceiverSocketId(receiverId);
+		if (receiverSocketId) {
+			//io.to(<socket_id>).emit() used to send events to specific client
+			io.to(receiverSocketId).emit("newMessage", newMessage);
+		}
 
 		res.status(201).json(newMessage);
 	} catch (error) {
 		console.log("Error in sendMessage controller: ", error.message);
 		res.status(500).json({ error: "Internal server error" });
 	}
-   // console.log("message sent");
+    console.log("message sent");
 };
 
 export const getMessages = async (req, res) => {
--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-//import path from "path";
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -8,11 +8,11 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
-//import { app, server } from "./socket/socket.js";
-const app = express();
+import { app, server } from "./socket/socket.js";
+
 const PORT = process.env.PORT || 5000;
 
-//const __dirname = path.resolve();
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -23,17 +23,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-//app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-//app.get("*", (req, res) => {
-	//res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-//});
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
 	connectToMongoDB();
 	console.log(`Server Running on port ${PORT}`);
 });
-/*server.listen(PORT, () => {
-	connectToMongoDB();
-	console.log(`Server Running on port ${PORT}`);
-});*/
